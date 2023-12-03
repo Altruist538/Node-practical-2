a98ab147986ba7cd64c48106fc34e47ca533115f
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-12-03 11:57:13 +0200

    HW-4

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const express = require("express");
 const connectDb = require("../config/connectDb");
 const errorHandler = require("./middlewares/errorHandler");
 const notFound = require("./middlewares/notFound");
+const asyncHandler = require("express-async-handler");
 require("colors");
+const User = require("./models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -12,6 +16,74 @@ app.use(express.json());
 
 app.use("/api/v1", require("./routes/carsRoutes"));
 
+// регистрация - сохранение пользователя в базе
+
+app.post(
+  "/register",
+  asyncHandler(async (req, res) => {
+    //Отримаємо и валідуємо данні від користувача
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400);
+      throw new Error(`Передайте все обязательные поля!!`);
+    }
+    //Шукаємо користувача в базі
+    const user = await User.findOne({ email });
+    //Якшо знайшли то відаємо помилку
+    if (user) {
+      res.status(400);
+      throw new Error("Пользователь уже существует");
+    }
+
+    //Якшо не знайшли то хешируємо пароль
+    const hashPassword = bcrypt.hashSync(password, 5);
+
+    //Зберігаемо в базі з захишованим паролем
+    const newUser = await User.create({ ...req.body, password: hashPassword });
+    res.status(201).json({ code: 201, data: { email: newUser.email } });
+  })
+);
+
+// аутинтефиция - проверка подлиности пользователя и пароля с тем что хранится в базе
+
+app.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    //Отримаємо и валідуємо данні від користувача
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400);
+      throw new Error(`Передайте все обязательные поля!!`);
+    }
+    //   //Шукаємо користувача в базі і розшифруємо пароль
+    const user = await User.findOne({ email });
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      res.status(400);
+      throw new Error(`Невірний логін або пароль`);
+    }
+
+    //   //Якшо знайшли і розшифрували
+    const token = generateToken({
+      students: ["Andrii", "Artem", "Pavlo"],
+      id: user._id,
+    });
+    //   //Зберігаемо в базі token
+
+    user.token = token;
+    await user.save();
+    res
+      .status(200)
+      .json({ code: 200, data: { email: user.email, token: user.token } });
+  })
+);
+
+function generateToken(data) {
+  const payload = { ...data };
+  return jwt.sign(payload, "cat", { expiresIn: "1d" });
+}
+
+// авторизация - проверка прав доступа
+// логаут - выход из системы
 app.use("*", notFound);
 
 app.use(errorHandler);
